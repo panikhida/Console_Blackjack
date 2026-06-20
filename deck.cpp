096a9670d84639fce5cc6ebc60ebdb50e7de7124
panikhida <gralbby@gmail.com>
2026-06-21 01:17:38 +0200

    vector card, print deck

--- deck.cpp
+++ deck.cpp
@@ -1,2 +1,12 @@
 #include "deck.h"
+#include "includes.h"
+std::vector<Card> deck (deck_size);
 
+void printDeck(const std::vector<Card>& deck) {
+    if (deck.empty()) {
+        std::cout << "Deck is empty" << "\n";
+    }
+    else {
+        
+    }
+}
--- deck.h
+++ deck.h
@@ -3,4 +3,6 @@
 
 constexpr int deck_size { 52 };
 
+
+
 #endif //CONSOLE_BLACKJACK_DECK_H
