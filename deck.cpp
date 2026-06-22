2bac0ac2901d3c23cf4add1f533d3a2aae02ce90
panikhida <gralbby@gmail.com>
2026-06-22 12:12:34 +0200

    printDeck

--- deck.cpp
+++ deck.cpp
@@ -7,6 +7,10 @@ void printDeck(const std::vector<Card>& deck) {
         std::cout << "Deck is empty" << "\n";
     }
     else {
-        
+        for (auto card : deck)
+            {
+            std::cout << card.rank << card.suit << ", Value == " << card.value << ". \n";
+            std::cout << "\tCapacity: " << deck.capacity() << "Length: " << deck.size() << ". \n";
+        }
     }
 }
