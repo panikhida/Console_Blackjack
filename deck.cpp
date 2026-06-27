71922d3bf7278e14732e2fc5652711f4a744b633
panikhida <gralbby@gmail.com>
2026-06-28 00:52:08 +0200

    fillDeck

--- deck.cpp
+++ deck.cpp
@@ -2,28 +2,29 @@
 #include "deck.h"
 //┌ ─ ┐
 //│
-//└─┘
-void printDeck(const std::vector<Card>& d) {
-    if (d.empty()) {
-        std::cout << "Deck is empty" << "\n";
-    }
-    else {
-        std::cout << "          DECK         \n";
-        std::cout << "==========----==========\n";
-        for (auto card : d)
-            {
-            if (card.value > 9 && card.rank == Rank::ten) {
-                std::cout << "││   " << card.rank << card.suit << ", Value == " << card.value << " ││\n";
-            } else if (card.value > 9 || card.rank == Rank::ten) {
-                    std::cout << "││   " << card.rank << card.suit << ", Value == " << card.value << "  ││\n";
-                } else {
-                    std::cout << "││   " << card.rank << card.suit << ", Value == " << card.value << "   ││\n";
-                }
+//└ ─ ┘
+    void printDeck(const std::vector<Card>& d) {
+        if (d.empty()) {
+            std::cout << "Deck is empty" << "\n";
         }
-        std::cout << "│Capacity: " << d.capacity() << " Length: " << d.size() << " │\n";
-        std::cout << "└──────────────────────┘\n";
+        else {
+            std::cout << "          DECK         \n";
+            std::cout << "==========----==========\n";
+            for (auto card : d)
+                {
+                if (card.value > 9 && card.rank == Rank::ten) {
+                    std::cout << "││   " << card.rank << card.suit << ", Value == " << card.value << " ││\n";
+                } else if (card.value > 9 || card.rank == Rank::ten) {
+                        std::cout << "││   " << card.rank << card.suit << ", Value == " << card.value << "  ││\n";
+                    } else {
+                        std::cout << "││   " << card.rank << card.suit << ", Value == " << card.value << "   ││\n";
+                    }
+            }
+            std::cout << "│Capacity: " << d.capacity() << " Length: " << d.size() << " │\n";
+            std::cout << "└──────────────────────┘\n";
+        }
     }
-}
-void fillDeck(std::vector<Card>& d) {
+    void fillDeck(std::vector<Card>& d) {
+        std::string {"string"};
 
-}
+    }
