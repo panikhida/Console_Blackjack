27e1ad2c4ce4323b42a6bc80f909b5ca5aeeb798
panikhida <gralbby@gmail.com>
2026-07-03 01:13:49 +0200

    game working

--- game.cpp
+++ game.cpp
@@ -80,27 +80,70 @@ void blackjack()
         std::cout << "Dealer's visible deck value is " << calcDeckValue(deck_dlr, 1) << "\n";
         std::cout << "Do you want to take another card?(0/1): ";
         int aif = askIfContinue();
-            while (aif == 1 && deck_plr.size() < 10) {
+            while (aif == 1 && calcDeckValue(deck_plr) < 21) {
                 giveCard(deck, deck_plr);
                 std::cout << "Dealer gave you a card:\n";
                 std::cout << "Your deck updated:\n";
                 printDeck(deck_plr, 'p');
                 std::cout << "Your deck value is " << calcDeckValue(deck_plr) << " points\n";
                 std::cout << "Dealer's visible deck value is " << calcDeckValue(deck_dlr, 1) << "\n";
-
                 std::cout << "Do you want to take another card?(0/1): ";
                 aif = askIfContinue();
+                }
             }
         std::cout << "The dealer opens another card:\n";
         printDeck(deck_dlr, 'd');
         std::cout << "Dealer has " << calcDeckValue(deck_dlr) << " points\n";
         while (calcDeckValue(deck_dlr) < 13) {
             std::cout << "It is less than 13, dealer takes another card.";
+            giveCard(deck, deck_dlr);
         }
+
         std::cout << "\n";
+        std::cout << "Calculating winner...\n";
+        const int f_dlr {calcDeckValue(deck_dlr)};
+        const int f_plr {calcDeckValue(deck_plr)};
+
+        char winner {};
+
+        if (f_plr > 21) {
+            winner = 'd';
+        }
+        if (f_dlr == 21 && f_plr != 21) {
+            winner = 'd';
+        }
+        if (f_dlr < 21 && f_plr < f_dlr) {
+            winner = 'd';
+        }
+        if (f_dlr > 21) {
+            winner = 'p';
+        }
+        if (f_plr == 21 && f_dlr != 21) {
+            winner = 'p';
+        }
+        if (f_plr < 21 && f_dlr < f_plr) {
+            winner = 'p';
+        }
+        if (f_dlr == f_plr) {
+            winner = 't';
+        }
+        switch (winner) {
+            case 'd':
+                std::cout << "Dealer won, you lost your bid!\n";
+                cash = cash - bid;
+                break;
+            case 'p':
+                std::cout << "You won! You doubled your bid!\n";
+                cash = cash + bid;
+                break;
+            default:
+                std::cout << "It's a tie!\n";
+                break;
+        }
         //end
         deck.clear();
         deck_plr.clear();
         deck_dlr.clear();
     }
+    std::cout << "Game ended.";
 }
